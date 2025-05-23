41d3c2a97ba751958ce80b6b0f1d69e76372ad7b
Pedro Rivera <priveraa94@gmail.com>
2025-05-22 21:28:26 -0400

    adds functions and html

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 // functions
+
+let add = (a, b) => {
+  return a + b
+}
+
+let subtract = (a, b) => {
+  return a - b
+}
+
+let multiply = (a, b) => {
+  return a * b
+}
+
+let divide = (a, b) => {
+  return a / b
+}
+
+const firstNum = 0
+const operator = 0
+const secondNum = 0
+
+let operate = (operator, num1, num2) => {
+
+}
